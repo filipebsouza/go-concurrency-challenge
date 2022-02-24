1488607dd64d851e480ca58bcc3431908fab62d3
Filipe Bezerra de Souza <filipe.bsouza@gmail.com>
2022-02-24 14:25:58 -0400

    refactor: clean code and concurrency adjustments

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-	var wg sync.WaitGroup
 	rand.Seed(time.Now().UnixNano())
 	orderRequests := 0
 	for {
@@ -19,58 +18,22 @@ func main() {
 	}
 
 	log.Printf("Number of orders requested are %v", orderRequests)
+	wg := new(sync.WaitGroup)
 	wg.Add(orderRequests)
 	for i := 0; i < orderRequests; i++ {
 		go func() {
 			defer wg.Done()
-			numberOfBurgers, numberOfFries, numberOfBeverages, quantity := 0, 0, 0, 0
-			for {
-				numberOfBurgers = rand.Intn(10)
-				log.Printf("Number of Burgers are %v", numberOfBurgers)
-				numberOfFries = rand.Intn(10)
-				log.Printf("Number of Fries are %v", numberOfFries)
-				numberOfBeverages = rand.Intn(10)
-				log.Printf("Number of Beverages are %v", numberOfBeverages)
-				quantity = numberOfBurgers + numberOfFries + numberOfBeverages
 
-				if quantity > 0 {
-					break
-				}
-			}
-			products := make([]IProduct, quantity)
-			index := 0
-			for {
-				for j := 0; j < numberOfBurgers; j++ {
-					point := rand.Intn(WellDone-BlueRare) + BlueRare
-					salad := rand.Intn(CrispyFriedCabbage-WithoutSalad) + WithoutSalad
-					bacon := rand.Intn(Crumbs-WithoutBacon) + WithoutBacon
+			wgProduct := new(sync.WaitGroup)
+			cn := make(chan []IProduct, 1)
+			wgProduct.Add(1)
 
-					burger := RequestBurger(point, salad, bacon)
-					products[index] = burger
-					index++
-				}
+			go requestProducts(wgProduct, cn)
 
-				for j := 0; j < numberOfFries; j++ {
-					kind := rand.Intn(RusticFries-Normal) + Normal
+			wgProduct.Wait()
 
-					fries := RequestFries(kind)
-					products[index] = fries
-					index++
-				}
-
-				for j := 0; j < numberOfBeverages; j++ {
-					kind := rand.Intn(OrangeJuice-Coke) + Coke
-
-					beverage := RequestBeverage(kind)
-					products[index] = beverage
-					index++
-				}
-
-				if index == quantity {
-					break
-				}
-			}
-
+			products := <-cn
+			close(cn)
 			order := MakeAnOrder(products...)
 			order.WaitFinishPreparation()
 		}()
@@ -80,7 +43,8 @@ func main() {
 	log.Println("All orders finished")
 }
 
-func requestProducts() []IProduct {
+func requestProducts(wg *sync.WaitGroup, result chan []IProduct) {
+	defer wg.Done()
 	numberOfBurgers, numberOfFries, numberOfBeverages, quantity := 0, 0, 0, 0
 	for {
 		numberOfBurgers = rand.Intn(10)
@@ -95,49 +59,26 @@ func requestProducts() []IProduct {
 			break
 		}
 	}
-	products := make([]IProduct, quantity)
-	index := 0
-	for {
-		var wg sync.WaitGroup
-		wg.Add(3)
-		burgers := make([]IProduct, numberOfBurgers)
-		go requestBurgers(wg, numberOfBurgers, &burgers)
+	wgProducts := new(sync.WaitGroup)
+	wgProducts.Add(3)
+	burgers := make([]IProduct, numberOfBurgers)
+	fries := make([]IProduct, numberOfFries)
+	beverages := make([]IProduct, numberOfBeverages)
 
-		for j := 0; j < numberOfBurgers; j++ {
-			point := rand.Intn(WellDone-BlueRare) + BlueRare
-			salad := rand.Intn(CrispyFriedCabbage-WithoutSalad) + WithoutSalad
-			bacon := rand.Intn(Crumbs-WithoutBacon) + WithoutBacon
+	go requestBurgers(wgProducts, numberOfBurgers, &burgers)
+	go requestFries(wgProducts, numberOfFries, &fries)
+	go requestBeverages(wgProducts, numberOfBeverages, &beverages)
 
-			burger := RequestBurger(point, salad, bacon)
-			products[index] = burger
-			index++
-		}
+	wgProducts.Wait()
 
-		for j := 0; j < numberOfFries; j++ {
-			kind := rand.Intn(RusticFries-Normal) + Normal
+	products := burgers
+	products = append(products, fries[:]...)
+	products = append(products, beverages[:]...)
 
-			fries := RequestFries(kind)
-			products[index] = fries
-			index++
-		}
-
-		for j := 0; j < numberOfBeverages; j++ {
-			kind := rand.Intn(OrangeJuice-Coke) + Coke
-
-			beverage := RequestBeverage(kind)
-			products[index] = beverage
-			index++
-		}
-
-		if index == quantity {
-			break
-		}
-	}
-
-	return products
+	result <- products
 }
 
-func requestBurgers(wg sync.WaitGroup, numberOfBurgers int, burgers *[]IProduct) interface{} {
+func requestBurgers(wg *sync.WaitGroup, numberOfBurgers int, burgers *[]IProduct) {
 	defer wg.Done()
 	for i := 0; i < numberOfBurgers; i++ {
 		point := rand.Intn(WellDone-BlueRare) + BlueRare
@@ -147,6 +88,24 @@ func requestBurgers(wg sync.WaitGroup, numberOfBurgers int, burgers *[]IProduct)
 		burger := RequestBurger(point, salad, bacon)
 		(*burgers)[i] = burger
 	}
+}
 
-	return burgers
+func requestFries(wg *sync.WaitGroup, numberOfFries int, fries *[]IProduct) {
+	defer wg.Done()
+	for i := 0; i < numberOfFries; i++ {
+		kind := rand.Intn(RusticFries-Normal) + Normal
+
+		f := RequestFries(kind)
+		(*fries)[i] = f
+	}
+}
+
+func requestBeverages(wg *sync.WaitGroup, numberOfBeverages int, beverages *[]IProduct) {
+	defer wg.Done()
+	for i := 0; i < numberOfBeverages; i++ {
+		kind := rand.Intn(OrangeJuice-Coke) + Coke
+
+		beverage := RequestBeverage(kind)
+		(*beverages)[i] = beverage
+	}
 }
